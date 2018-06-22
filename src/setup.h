abf11d42bd7853b963996216f0ee7078e7791d67
jose <jose.kahan@w3.org>
2018-06-22 13:49:32 +0200

    Rather than converting multipart/alternative to multipart/mixed, use hypermail's set_save_alts to deal with Apple Mail. Cleaner. The Apple Mail UA string depends on the device used. set_apple_mail_ua is now a list where you can add new device names. The default values are Apple and iPhone. These are concatenated with "Mail (" when doing a UA match.

--- src/setup.h
+++ src/setup.h
@@ -96,7 +96,6 @@ extern bool set_use_sender_date;
 extern bool set_inline_addlink;
 extern bool set_applemail_mimehack;
 extern char *set_applemail_ua_header;
-extern char *set_applemail_ua_value;
 
 extern int set_showhtml;
 extern int set_thrdlevels;
@@ -143,6 +142,7 @@ extern struct hmlist *set_filter_out;
 extern struct hmlist *set_filter_require;
 extern struct hmlist *set_filter_out_full_body;
 extern struct hmlist *set_filter_require_full_body;
+extern struct hmlist *set_applemail_ua_value;
 
 extern char *set_ihtmlheader;
 extern char *set_ihtmlfooter;
